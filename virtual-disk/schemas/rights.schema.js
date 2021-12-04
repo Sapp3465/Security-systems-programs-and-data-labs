ce3fcc77a0ce3515cc0627ffcfd2930f7ffb0dbc
BohdanShmalko <43369238+Sapp3465@users.noreply.github.com>
2021-12-04 23:09:19 +0200

    lab 1 all files

--- virtual-disk/schemas/rights.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RightsModel = exports.RightsSchema = void 0;
-var mongoose_1 = require("mongoose");
-exports.RightsSchema = new mongoose_1.Schema({
-    label: { type: String, required: true }
-});
-exports.RightsModel = (0, mongoose_1.model)('Rights', exports.RightsSchema);
